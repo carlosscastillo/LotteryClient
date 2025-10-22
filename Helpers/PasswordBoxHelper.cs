5f6810531cf80392fa28514acce5da2ac614c663
CARLOS CASTILLO BARRADAS <zS23014088@estudiantes.uv.mx>
2025-10-21 22:45:55 -0600

    Actualizamos la vista de inicio y añadimos PasswordBoxHelper

---
+++ Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// Se usa para enlazar la propiedad Password de un PasswordBox en MVVM
+namespace Lottery.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword",
+                typeof(string), typeof(PasswordBoxHelper),
+                new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(
+            DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox box)
+            {
+                box.PasswordChanged -= HandlePasswordChanged;
+                box.Password = (string)e.NewValue;
+                box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox box)
+            {
+                SetBoundPassword(box, box.Password);
+            }
+        }
+    }
+}
